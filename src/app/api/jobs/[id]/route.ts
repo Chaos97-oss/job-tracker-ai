780b45b51142950fbe67de816b9d9dfedb7ed3c8
Paul <udegbuepaul43@gmail.com>
2025-06-20 17:12:25 +0100

    final fix (again)

--- src/app/api/jobs/[id]/route.ts
+++ src/app/api/jobs/[id]/route.ts
@@ -1,30 +1,27 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { readJobs, writeJobs } from "@/lib/jobsStorage";
 import { Job } from "@/types/job";
 
-type Params = {
+type RouteParams = {
   params: {
     id: string;
   };
 };
 
-export async function PUT(req: Request, { params }: Params) {
+export async function PUT(req: NextRequest, { params }: RouteParams) {
   const body = await req.json();
   const jobs: Job[] = readJobs();
-
   const updatedJobs = jobs.map((job) =>
     job.id === params.id ? { ...job, ...body } : job
   );
 
   writeJobs(updatedJobs);
-
   return NextResponse.json({ message: "Updated successfully" });
 }
 
-export async function DELETE(_: Request, { params }: Params) {
+export async function DELETE(_: NextRequest, { params }: RouteParams) {
   const jobs: Job[] = readJobs();
   const filteredJobs = jobs.filter((job) => job.id !== params.id);
-
   writeJobs(filteredJobs);
 
   return NextResponse.json({ message: "Deleted successfully" });
